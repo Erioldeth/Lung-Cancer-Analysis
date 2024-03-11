29feaf105dae5aef2258b6364199aa04e4f511aa
Nguyen Hai Dan <haidan10112003@gmail.com>
2024-03-11 21:55:29 +0700

    using opencv

--- preprocessing _img.py
+++ preprocessing _img.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import os
+import cv2
 
 # Define directory containing folders
 data_dir = "C:/Users/ADMIN/Downloads/archive/CT"
@@ -10,7 +11,7 @@ cancer_dir = "cancer"
 
 output_dir = "G:\Learn\lung_cancer_analysis\data"  # Modify as needed
 
-target_resolution = (224, 224)
+target_resolution = (256, 256)
 
 # Create output folders if they don't exist
 os.makedirs(os.path.join(output_dir, normal_dir), exist_ok=True)
@@ -24,9 +25,9 @@ for folder in [normal_dir, cancer_dir]:
     for filename in os.listdir(folder_path):
         # Check for PNG or JPG extensions
         if filename.endswith(".png") or filename.endswith(".jpg"):
-            img = Image.open(os.path.join(folder_path, filename))
-            img = img.resize(target_resolution)
+            img = cv2.imread(os.path.join(folder_path, filename))
+            img = cv2.resize(img, target_resolution)
             new_filename = os.path.splitext(filename)[0] + ".jpg"
             new_filepath = os.path.join(output_dir, folder, new_filename)
-            img.save(new_filepath)
+            cv2.imwrite(new_filepath, img)
             print(f"Converted {folder}/{filename} to {new_filepath}")
